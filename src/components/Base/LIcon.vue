9cbb017bc3a106fd8962c12b9f8e019415c9c4dc
unknown <denis.puma@unap.edu.pe>
2023-10-10 16:20:47 -0500

    l-list-item icon

--- src/components/Base/LIcon.vue
+++ src/components/Base/LIcon.vue
@@ -1,25 +1,40 @@
 <template>
   <svg
+    :class="`w-${size} h-${size}`"
+    class="w-"
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
     stroke-width="1.5"
     stroke="currentColor"
-    class="w-5 h-5"
+    aria-hidden="true"
   >
     <path
       stroke-linecap="round"
       stroke-linejoin="round"
-      :d="path"
-    />
+      :d="icons.filter((item) => item.name === icon)[0].icon"
+    ></path>
   </svg>
 </template>
 
 <script setup>
+import icons from "@/assets/icons.json";
 const props = defineProps({
-  path: {
+  icon: {
     type: String,
     required: true,
+    validator: function (value) {
+      // Define una lista de valores permitidos
+      const allowedItems = icons.map((item) => item.name);
+
+      // Comprueba si el valor del prop se encuentra en la lista permitida
+      return allowedItems.includes(value);
+    },
+  },
+
+  size: {
+    type: [Number, String],
+    default: 5,
   },
 });
 </script>
---
+++ src/components/Base/LListItem.vue
@@ -0,0 +1,48 @@
+<template>
+  <div
+    class="relative w-full   inline-flex justify-between py-2 px-3 items-center"
+    :class=" dark ? 'text-white' : 'text-gray-900' "
+  >
+    <router-link
+      v-if="link"
+      class="absolute top-0 left-0 w-full h-full"
+      :to="href"
+    />
+    <slot v-if="$slots['prependIcon']" name="prependIcon" />
+    <div v-if="prependIcon && !$slots['prependIcon']" class="me-3">
+      <LIcon :icon="prependIcon" />
+    </div>
+
+    <div class="w-full">
+      <div v-if="title" class="text-sm text-ellipsis font-medium">
+        {{ title }}
+      </div>
+      <div v-if="subtitle" class="text-sm text-gray-500">
+        {{ subtitle }}
+      </div>
+      <slot />
+    </div>
+
+    <slot v-if="$slots['appendIcon']" name="appendIcon" />
+
+    <div v-if="appendIcon && !$slots['appendIcon']" class="ms-3">
+      <LIcon :icon="appendIcon" />
+    </div>
+  </div>
+</template>
+<script setup>
+import LIcon from "./LIcon.vue";
+
+const props = defineProps({
+  title: String,
+  subtitle: String,
+  prependIcon: String,
+  appendIcon: String,
+  dark: Boolean,
+  link: {
+    type: Boolean,
+    default: false,
+  },
+  href: String,
+});
+</script>
